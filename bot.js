a1d018dd7147a1c6cd1ffaf017397cb26c7d3ba0
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-11 08:35:02 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -194,9 +194,9 @@ client.on("guildMemberAdd", member => {
  
 
 client.on("message", msg=>{
-let id = "434282754016935937"; // ايديك
+let id = "491884648276819968"; // ايديك
 let role = "VIP"; // اسم رتبة الفيب
-let Price = 10; // السعر
+let Price = 100; // السعر
 let Price2 = Math.floor(Price-(Price*(1/100)));
 if(!Price || Price < 1) return;
 let cmd = msg.content.split(' ')[0];
@@ -357,6 +357,230 @@ function saveChanges() {
         if (error) console.log(error);
     });
 }
+
+
+
+let points = JSON.parse(fs.readFileSync('./points.json' , 'utf8')); //Toxic Codes // n3k4a is one
+if(!points.json) points.json = {}
+client.on('message', async message => {//Toxic Codes // n3k4a is one
+ 
+ 
+    if(message.channel.type !== 'text') return;
+   
+   
+    var command = message.content.toLowerCase().split(" ")[0];//Toxic Codes // n3k4a is one
+    var args = message.content.toLowerCase().split(" ");
+    var userM = message.guild.member(message.mentions.users.first() || message.guild.members.find(m => m.id == args[1]));
+      const embed  = new Discord.RichEmbed()
+.setDescription(`
+**لم يتم تسجيل أي نقطة حتى الأن **
+ 
+** أمثلة للأوامر: **
+**:small_orange_diamond:** ${prefix}points ${message.author} 1 \`لتغيير نقاط شخص معين \`
+**:small_orange_diamond:** ${prefix}points ${message.author} +1 \`لزيادة نقاط شخص معين\`
+**:small_orange_diamond:** ${prefix}points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
+**:small_orange_diamond:** ${prefix}points ${message.author} 0 \`لتصفير نقاط شخص معين \`
+**:small_orange_diamond:** ${prefix}points reset \`لتصفير جميع النقاط\`
+`)
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setColor(`#e60909`)
+  const error  = new Discord.RichEmbed()
+.setDescription(`
+**:x: | يجب كتابة الأمر بشكل صحيح. **
+ 
+** أمثلة للأوامر: **
+**:small_orange_diamond:** ${prefix}points ${message.author} 1 \`لتغيير نقاط شخص معين \`
+**:small_orange_diamond:** ${prefix}points ${message.author} +1 \`لزيادة نقاط شخص معين\`
+**:small_orange_diamond:** ${prefix}points ${message.author} -1 \`لأزالة نقطة من شخص معين \`
+**:small_orange_diamond:** ${prefix}points ${message.author} 0 \`لتصفير نقاط شخص معين \`
+**:small_orange_diamond:** ${prefix}points reset \`لتصفير جميع النقاط\``)
+.setFooter('Requested by '+message.author.username, message.author.avatarURL)
+.setColor(`#e60909`)
+if(command == prefix + 'points') {
+     
+        if(!message.guild.member(client.user).hasPermission('EMBED_LINKS')) return message.channel.send(':no_entry: | I dont have Embed Links permission.');
+        if(!args[1]) {
+            if(!points) return message.channel.send(embed);
+            var members = Object.values(points);
+            var memb = members.filter(m => m.points >= 1);
+            if(memb.length == 0) return message.channel.send(embed);
+            var x = 1;
+            let pointsTop = new Discord.RichEmbed()
+            .setAuthor('Points:')
+            .setColor('#FBFBFB')
+            .setDescription(memb.sort((second, first) => first.points > second.points).slice(0, 10).map(m => `**:small_blue_diamond:** <@${m.id}> \`${m.points}\``).join('\n'))
+            .setFooter(`Requested by ${message.author.username}`, message.author.avatarURL);
+            message.channel.send({
+                embed: pointsTop
+            });
+        }else if(args[1] == 'reset') {
+            let pointsReset = new Discord.RichEmbed()
+            .setDescription('**:white_check_mark: | تم تصفير جميع النقاظ بنجاح**')//Toxic Codes // n3k4a is one
+            .setFooter('Requested by '+message.author.username, message.author.avatarURL)//Toxic Codes // n3k4a is one
+            if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send("You dont have Manage Server permission.");
+            if(!points) return message.channel.send(pointsReset);//Toxic Codes // n3k4a is one
+            var members = Object.values(points);
+            var memb = members.filter(m => m.points >= 1);
+            if(memb.length == 0) return message.channel.send(pointsReset);
+            points = {};
+            message.channel.send(pointsReset);
+        }else if(userM) {
+            if(!message.member.hasPermission('MANAGE_GUILD')) return  message.channel.send("You dont have Manage Server permission.");
+            if(!points[userM.user.id]) points[userM.user.id] = {
+                points: 0,
+                id: userM.user.id
+            };
+            if(!args[2]) {
+                if(points[userM.user.id].points == 0) return message.channel.send( `${userM.user.username} Not have any points.`);
+                var userPoints = new Discord.RichEmbed()
+                .setColor('#d3c325')
+                .setAuthor(`${userM.user.username} have ${points[userM.user.id].points} points.`);
+                message.channel.send({
+                    embed: userPoints
+                });
+            }else if(args[2] == 'reset') {
+                if(points[userM.user.id].points == 0) return message.channel.send(error);
+                points[userM.user.id].points = 0;
+                message.channel.send(`**Successfully reset ${userM.user.username} points.**`);
+            }else if(args[2].startsWith('+')) {
+                args[2] = args[2].slice(1);
+                args[2] = parseInt(Math.floor(args[2]));
+                if(points[userM.user.id].points == 1000000) return message.channel.send(error);
+                if(!args[2]) return message.channel.send(error);
+                if(isNaN(args[2])) return message.channel.send(error);
+                if(args[2] > 1000000) return message.channel.send(error);
+                if(args[2] < 1) return message.channel.send(error);
+                if((points[userM.user.id].points + args[2]) > 1000000) args[2] = 1000000 - points[userM.user.id].points;
+                points[userM.user.id].points += args[2];
+                let add = new Discord.RichEmbed()
+                .setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+                .setAuthor('Points:')
+                .setColor('#FBFBFB')
+                .setFooter('Requested by' + message.author.username, message.author.avatarURL)
+                message.channel.send(add);
+            }else if(args[2].startsWith('-')) {
+                args[2] = args[2].slice(1);
+                args[2] = parseInt(Math.floor(args[2]));
+                if(points[userM.user.id].points == 0) return message.channel.send(error);
+                if(!args[2]) return message.channel.send(error);
+                if(isNaN(args[2])) return message.channel.send(error);
+                if(args[2] > 1000000) return message.channel.send(error);
+                if(args[2] < 1) return message.channel.send(error);
+                if((points[userM.user.id].points - args[2]) < 0) args[2] = points[userM.user.id].points;
+                points[userM.user.id].points -= args[2];
+                    let rem = new Discord.RichEmbed()
+                .setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+                .setAuthor('Points:')
+                .setColor('#FBFBFB')
+                .setFooter('Requested by' + message.author.username, message.author.avatarURL)
+                message.channel.send(rem);
+            }else if(!args[2].startsWith('+') || !args[2].startsWith('-')) {
+                args[2] = parseInt(Math.floor(args[2]));
+                if(isNaN(args[2])) return message.channel.send(error);
+                if(args[2] > 1000000) return message.channel.send(error);
+                if(args[2] < 1) return message.channel.send(error);
+                if(points[userM.user.id].points == args[2]) return message.channel.send(`${userM.user.username} points is already ${args[2]}.`);
+                points[userM.user.id].points = args[2];
+                    let set = new Discord.RichEmbed()
+                .setDescription(`**:small_blue_diamond:** <@${userM.id}> \`${points[userM.user.id].points}\``)
+                .setAuthor('Points:')
+                .setColor('#FBFBFB')
+                .setFooter('Requested by' + message.author.username, message.author.avatarURL)
+                message.channel.send(set);
+            }
+            }
+            }
+      fs.writeFile("./points.json", JSON.stringify(points) ,(err) =>{//Toxic Codes // n3k4a is one
+          if (err) console.log(err.message);
+      });//Toxic Codes // n3k4a is one
+ 
+});//Toxic Codes // n3k4a is one
+
+
+
+const cool = [];
+client.on('message',async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === 'dm') return;
+ 
+  const args = message.content.split(' ');
+  const credits = require('./credits.json');
+  const path = './credits.json';
+  const mention = message.mentions.users.first() || client.users.get(args[1]) || message.author;
+  const mentionn = message.mentions.users.first() || client.users.get(args[1]);
+  const author = message.author.id;
+  const balance = args[2];
+  const daily = Math.floor(Math.random() * 350) + 10;
+ 
+  if(!credits[author]) credits[author] = {credits: 50};
+  if(!credits[mention.id]) credits[mention.id] = {credits: 50};
+  fs.writeFile(path, JSON.stringify(credits, null, 5), function(err) {if(err) console.log(err)});
+ 
+ 
+  if(message.content.startsWith(prefix + "credit")) {
+  if(args[0] !== `${prefix}credit` && args[0] !== `${prefix}credits`) return;
+ 
+  if(args[2]) {
+    if(isNaN(args[2]) || args[2] < 0) return message.channel.send(`:interrobang: **| ${message.author.username}, type the credit you need to transfer! **`);
+    if(mention.bot) return message.channel.send(`**:heavy_multiplication_x:| ${message.content.split(' ')[1]} لم يتم العثور على**`);
+    if(mention.id === message.author.id) return message.channel.send('**:heavy_multiplication_x:| لا يمكنك تحويل كردت لنفسك**');
+    if(credits[author].credits < balance) return message.channel.send(`** :thinking: | ${message.author.username}, Your balance is not enough for that!**`);
+    var one = Math.floor(Math.random() * 9) + 1;
+    var two = Math.floor(Math.random() * 9) + 1;
+    var three = Math.floor(Math.random() * 9) + 1;
+    var four = Math.floor(Math.random() * 9) + 1;
+ 
+    var number = `${one}${two}${three}${four}`;
+   
+    message.channel.send(`**:heavy_dollar_sign:| \`${number}\`, أكتب الرقم للأستمرار**`).then(m => {
+      message.channel.awaitMessages(m => m.author.id === message.author.id, {max: 1, time: 10000}).then(c => {
+        if(c.first().content === number) {
+          m.delete();
+          message.channel.send(`**:moneybag: | ${message.author.username}, has transferred \`${balance}\` to ${mention}**`);
+          credits[author].credits += (-balance);
+          credits[mention.id].credits += (+balance);
+          fs.writeFile(path, JSON.stringify(credits, null, 5), function(err) {if(err) console.log(err)});
+        } else if(c.first().content !== number) {
+          m.delete();
+          message.channel.send(`** :money_with_wings: | تم الغاء الإرسال**`);
+        }
+      });
+    });
+  }
+  if(!args[2]) {
+    if(mention.bot) return message.channel.send(`:interrobang:**| ${message.author.username}, I can't find** ${message.content.split(' ')[1]}**!**`);
+    message.channel.send(`**${mention.username}, your :credit_card: balance is** \`$${credits[mention.id].credits}\`**.** `);
+  }
+ 
+  }
+        if(args[0].toLowerCase() === `${prefix}daily`) {  
+     
+if(credits[message.author.id].daily != moment().format('L')) {
+ 
+       credits[message.author.id].daily = moment().format('L');
+           
+ 
+          let ammount = (300, 500, 100, 200, 120, 150, 350, 320,220,250);
+          credits[author].credits += ammount;
+       
+       
+          message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${ammount} daily credits!**`);
+        fs.writeFile("./credits.json", JSON.stringify(credits), function(e) {
+            if (e) throw e;
+        })
+ 
+      }else{
+      message.channel.send(`:stopwatch: : **Please cool down  ${moment().endOf('day').fromNow()}**`);
+ 
+      }
+   
+        }
+         
+   
+ 
+});
+
+
 
 
 
