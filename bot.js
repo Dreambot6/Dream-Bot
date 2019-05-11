e505adbe04dd2db45ba442409ac6c2465b829fb7
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-11 07:26:53 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -110,6 +110,113 @@ bot.on('message', msg => {
   }
 })
 
+client.on("message",async message => {
+if(message.content === '=shop'){//الامر
+let staff = message.guild.member(message.author).roles.find('name' ,"Sellers");
+      if(!staff) return message.reply(`**Only Sellers | :x:**`)
+var shopc = message.guild.channels.find("name","「✮shop✮」")
+  if(!shopc) return message.reply("لا اجد الروم المخصص للبيع")
+    let shop = '';
+      let fillter = m => m.author.id === message.author.id
+     
+     
+ 
+      await message.channel.send("اكتب الاشياء الذي سوف تبيعها").then(e => {
+           message.channel.awaitMessages(fillter, { time: 60000, max: 1                                    
+})
+     .then(co => {
+       shop = co.first().content;
+        co.first().delete();
+     
+let desc = '';
+       
+e.edit("اكتب الدفع عند مين؟").then(e => {
+  message.channel.awaitMessages(fillter, { time: 60000, max: 1 })
+ 
+     .then(co => {
+       desc = co.first().content;
+        co.first().delete();
+e.edit("Done").then(e => {
+  shopc.send(` <$>
+${message.guild.name}:tm: Shop :arrow_down:
+======================
+${shop}
+=================
+**الدفع عند:** **${desc}**
+ 
+**تم الارسال بواسطة:** ${message.author}
+ @everyone</>@here `)
+  })
+})
+  })
+})
+  })
+           
+     
+ 
+     
+ 
+     
+           
+}
+});
+
+
+client.on("message", (message) => {
+ 
+   if (message.content.startsWith("=new")) {  
+        const reason = message.content.split(" ").slice(1).join(" ");  
+        if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`لازم تسوي رتبة اسمها \`Support Team\` وتنطي البوت ادمنيتر حتا يقدر يسوي الرومات ويعدل برمشنات`);
+        if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You already have a ticket open.`);    /// ALPHA CODES
+        message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+            let role = message.guild.roles.find("name", "Support Team");
+            let role2 = message.guild.roles.find("name", "@everyone");
+            c.overwritePermissions(role, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });  
+            c.overwritePermissions(role2, {
+                SEND_MESSAGES: false,
+                READ_MESSAGES: false
+            });
+            c.overwritePermissions(message.author, {
+                SEND_MESSAGES: true,
+                READ_MESSAGES: true
+            });
+            message.channel.send(`:white_check_mark: تم انشاء تذكرتك, #${c.name}.`);
+            const embed = new Discord.RichEmbed()
+                .setColor(0xCF40FA)
+                .addField(`Hey ${message.author.username}!`, `:white_check_mark:  تم انشاء تذكرتك, =ticket`)
+                .setTimestamp();
+            c.send({
+                embed: embed
+            });
+        }).catch(console.error);
+    }
+ 
+ 
+  if (message.content.startsWith("?close")) {
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+ 
+       message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب=confirm`)
+           .then((m) => {
+               message.channel.awaitMessages(response => response.content === '=confirm', {
+                       max: 1,
+                       time: 10000,
+                       errors: ['time'],
+                   })  
+                   .then((collected) => {
+                       message.channel.delete();
+                   })  
+                   .catch(() => {
+                       m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                           m2.delete();
+                       }, 3000);
+                   });
+           });
+   }
+ 
+});
 
 
 
