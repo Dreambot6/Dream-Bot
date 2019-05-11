d4a958aa1a2e135bc0d0046b25056a3fd99fb213
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-11 07:48:22 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -182,4 +182,179 @@ client.on("message", (message) => {
 
 
 
+
+﻿var Enmap = require('enmap');
+client.antibots = new Enmap({name: "antibot"});
+var antibots = client.antibots;
+var julian = client;
+julian.on("message", codes => {
+var prefix = "-";
+if(codes.content.startsWith(prefix + "antibots on")){
+if(codes.author.bot || !codes.channel.guild || codes.author.id != codes.guild.ownerID) return;
+antibots.set(`${codes.guild.id}`, {
+onoff: 'On'
+});
+ 
+ 
+codes.channel.send("AntiBots Join Is On");
+}
+if(codes.content.startsWith(prefix + "antibots off")){
+if(codes.author.bot || !codes.channel.guild || codes.author.id != codes.guild.ownerID) return;
+antibots.set(`${codes.guild.id}`, {
+onoff: "Off"
+});
+codes.channel.send("AntiBots Join Is Off");
+}
+});
+ 
+julian.on("guildMemberAdd", member => {
+if(!antibots.get(`${member.guild.id}`)) { antibots.set(`${member.guild.id}`, {
+onoff: "Off"
+});
+}
+if(antibots.get(`${member.guild.id}`).onoff == "Off") return;
+if(member.user.bot) return member.kick()
+});
+
+
+
+client.on("guildMemberAdd", member => {
+  client.channels.find('id', '576778852009902101').send(`**مرحبابك نتمني لك الأستمتاع في سيرفرنا : [ ${member} ]**`)
+});
+
+
+
+
+ 
+let vipKeys = JSON.parse(fs.readFileSync("./vipKeys.json", "utf8"));
+client.on("message", msg=>{
+let id = "491884648276819968"; // ايديك
+let role = "VIP"; // اسم رتبة الفيب
+let Price = 500; // السعر
+let Price2 = Math.floor(Price-(Price*(1/100)));
+if(!Price || Price < 1) return;
+let cmd = msg.content.split(' ')[0];
+if(cmd === `${prefix}buy`){
+if(msg.author.bot) return;
+if(!msg.channel.guild) return;
+let embedvip = new Discord.RichEmbed()
+.setColor("#42f4f4")
+.setAuthor(msg.author.username, msg.author.displayAvatarURL)
+.setThumbnail(msg.author.avatarURL)
+.setTitle("**اختر الطريقة المناسبة لك**")
+.addField("ل شراء الفي اي بي لنفسك","🔱",true )
+.addField("ل شراء الفي اي بي ك هدية","🎁",true)
+.setTimestamp()
+.setFooter(client.user.username,client.user.displayAvatarURL);
+msg.channel.send(embedvip).then(msgs2 =>{
+msgs2.react("🔱").then(()=>{
+  msgs2.react("🎁").then(()=>{
+    const me = (reaction, user) => reaction.emoji.name === '🔱' && user.id === msg.author.id;
+    const gift = (reaction, user) => reaction.emoji.name === '🎁' && user.id === msg.author.id;
+    const mec = msgs2.createReactionCollector(me, {time: 120000});
+    const giftc = msgs2.createReactionCollector(gift, {time: 120000});
+mec.on("collect", r=>{  
+msgs2.delete()
+if(msg.member.roles.find(r=>r.name == role)) return msg.reply("انت تمتلك الرتبة مسبقًا");
+let roleW = msg.guild.roles.find(r=>r.name == role);
+if(!roleW) return msg.reply(`البوت مقفل لعدم وجود رتبة ب أسم \`${role}\``)
+msg.channel.send(`كردت بروبوت\`${Price}\` لديك 4 دقائق لتحويل
+إلى ${msg.guild.members.get(id)}
+`).then( msgs =>{
+const filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`:moneybag: | ${msg.author.username}, has transferred \`$${Price2}\` to ${msg.guild.members.get(id)}`);
+msg.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
+.then( collected =>{
+msgs.delete()
+msg.reply(`تم اعطائك رتبة \`${role}\``);
+msg.member.addRole(roleW);
+}).catch(e => {});
+})})
+giftc.on("collect", r=>{
+  msgs2.delete()
+  let roleW = msg.guild.roles.find(r=>r.name == role);
+  if(!roleW) return msg.reply(`البوت مقفل لعدم وجود رتبة ب أسم \`${role}\``)
+msg.channel.send(`كردت بروبوت\`${Price}\` لديك 4 دقائق لتحويل
+إلى ${msg.guild.members.get(id)}
+`).then( msgs =>{
+  const filter = response => response.author.id == "282859044593598464" && response.mentions._content.includes(`:moneybag: | ${msg.author.username}, has transferred \`$${Price2}\` to ${msg.guild.members.get(id)}`);
+  msg.channel.awaitMessages(filter, { maxMatches: 1, time: 240000, errors: ['time'] })
+  .then( collected =>{
+  msgs.delete()
+  genKey(msg,roleW);
+  }).catch(e => {});
+  })
+})
+})})})
+///
+}
+if(cmd === `${prefix}used`){
+  let args = msg.content.split(" ").slice(1)[0];
+  if(!args) {  
+    let embed = new Discord.RichEmbed()
+    .setColor("#42f4f4")
+    .setTitle(`:x: - **الرجاء ادخال كود الهدية** \`${prefix}used <Key>\``)
+    msg.reply(embed).then( z => z.delete(3000));
+    return
+}
+  let embed = new Discord.RichEmbed()
+.setTitle(`**جاري التحقق من الكود**`)
+.setColor("#42f4f4")
+  msg.reply(embed).then( msgs =>{
+  if(vipKeys[args]){
+    let hav = msg.member.roles.find(`name`, vipKeys[args].name);
+    if(hav){
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **انت تمتلك هذه الرتبة مسبقًا**  \`${vipKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    return
+    }
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:tada: - **مبروك تم اعطائك رتبة** \`${vipKeys[args].name}\``)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+    msg.member.addRole(vipKeys[args]);
+    delete vipKeys[args]
+    save()
+  }else{
+    let embed = new Discord.RichEmbed()
+    .setTitle(`:x: - **الكود غير صيحيح أو انه مستعمل من قبل**`)
+    .setColor("#42f4f4")
+    msgs.edit(embed)
+  }});
+}
+});
+ 
+function genKey(msg,role){
+  var randomkeys = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var gift = "";
+  for (var y = 0; y < 16; y++) {   ///16
+    gift +=  `${randomkeys.charAt(Math.floor(Math.random() * randomkeys.length))}`;
+  }
+  vipKeys[gift] = role;
+  let embed = new Discord.RichEmbed()
+  .setColor("#42f4f4")
+  .setTitle(`:ok_hand: - **تم ارسآل الكود على الخاص**`);
+  msg.reply(embed);
+  let embed2= new Discord.RichEmbed()
+  .setAuthor(msg.author.username, msg.author.displayAvatarURL)
+  .setThumbnail(msg.author.avatarURL)
+  .addField("**Key Of Gift**", gift,true)
+  .addField("Role",vipKeys[gift].name,true)
+  .addField("This Key Made by", msg.author, true)
+  .addField("The Room", msg.channel , true)
+  .setTimestamp()
+  .setFooter(client.user.username,client.user.displayAvatarURL)  
+  msg.author.send(embed2);
+  save()
+}
+ 
+function save(){
+  fs.writeFile("./vipKeys.json", JSON.stringify(vipKeys), (err) => {
+    if (err) console.log(err)
+  });
+ 
+}
+
+
 client.login(process.env.BOT_TOKEN);
