6e56dcc6d6c708a565b22c9e508626c3eb7dbbf2
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-11 07:06:48 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,47 +50,6 @@ msg.reply(`resulting is ${resulting}$`)
 
 
 
-client.on("message", message => {
-    if(message.content.startsWith(prefix + "server")){
-        if(message.author.bot || message.channel.type == "dm") return;
-        let onlineM = message.guild.members.filter(m => m.presence.status !== "offline");
-        let verifyL = ["None", "Low", "Medium", "Hard", "Extreme"];
-        let region = {
-            'brazil': "`Brazil`",
-            'eu-central': "`Central Europe`",
-            'singapore': "`Singapore`",
-            'us-central': "`US Central`",
-            'sydney': "`Sydney`",
-            'us-east': "`US East`",
-            'us-south': "`US South`",
-            'us-west': "`US West`",
-            'eu-west': "`Western Europe`",
-            'london': "`London`",
-            'amsterdam': "`Amsterdam`",
-            'hongkong': "`Hong Kong`",
-            'russia': "`Russia`"
-        };
-        let serverEmbed = new Discord.RichEmbed()
-        .setColor("#36393e")
-        .setThumbnail(message.guild.iconURL)
-        .setAuthor(`${message.guild.name}, Server Info`)
-        .setDescription(`- Server Name \`${message.guild.name}\`\n- Created At \`${moment(message.guild.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Total Members \`${message.guild.memberCount} [Online: ${onlineM.size}]\`\n- Server Owner \`${message.guild.owner.user.tag}\`\n- Channels \`${message.guild.channels.filter(m => m.type == 'text').size} Text || ${message.guild.channels.filter(m => m.type == 'voice').size} Voice\`\n- Categories \`${message.guild.channels.filter(m => m.type == 'category').size}\`\n- Roles \`${message.guild.roles.size}\`\n- Region \`${region[message.guild.region]}\`\n- Verification Level \`${verifyL[message.guild.verificationLevel]}\`\n- Server ID \`${message.guild.id}\``)
-        .setFooter(message.client.user.username,message.client.user.avatarURL);
-    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`:no_entry: You dont have permission!`).then(mm => mm.delete(5000));  message.channel.send(serverEmbed).catch(console.error);
-    } else if(message.content.startsWith(prefix + "user")){
-        if(message.author.bot || message.channel.type == "dm") return;
-        let mnt = message.mentions.users.first();
-        let user = mnt || message.author;
-        let userEmbed = new Discord.RichEmbed()
-        .setColor("#36393e")
-        .setThumbnail(user.avatarURL)
-        .setAuthor(`${user.username}, User Info`)
-        .setDescription(`- Name \`${user.tag}\`\n- Created At \`${moment(user.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Joined At \`${moment(user.joinedAt).format('D/MM/YYYY h:mm a')}\`\n- ID \`${user.id}\``)
-        .setFooter(message.client.user.username,message.client.user.avatarURL);
-        message.channel.send(userEmbed).catch(console.error);
-       
-    }
-});
 
 client.on("message", message => {
 if(message.content.startsWith(prefix + "vmute")) {
@@ -112,5 +71,36 @@ message.channel.send(`Successfully Unmuted ${mnt} :+1:`)
 }).catch(console.error);
 }
 });
+
+
+﻿client.on("message", message => {
+if(message.content.startsWith(prefix + `contact`)){
+if(message.author.bot || message.channel.type == 'dm') return;
+let args = message.content.split(" ").slice(1);
+let msg = args.join(' ');
+let dev = client.users.get(""); //Your id
+if(!args) return message.reply("يجب كتابة الرسالة");
+dev.send(`• | User: **${message.author.tag}**\n\n• | Message: **${msg}**`).then(() =>{
+message.channel.send(`Your message has been successfully delivered to the bot owner`)
+}).catch(console.error);
+}
+});
+
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.on('guildCreate', guild => {
+  let support = client.guilds.get('576704119058726912') // حط هنا ايدي سيرفر السبورت
+  if(support === undefined) return
+  let role = support.roles.find(r => r.name == 'Member') // بدلها بأسم الرتبة يلي تبيها للمستخدمين
+  let member = support.members.get(guild.owner.user.id) 
+  if(member) {
+    member.addRole(role)
+  } else {
+    console.log(`this user not in support server`)
+  }
+})
+
+
 
 client.login(process.env.BOT_TOKEN);
