6e7046953086588a6c621533bbba77f57db2df39
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-11 06:52:25 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const prefix = '='
+ 
+ client.on('ready', () => {
+  client.user.setGame(`working .`,'https://www.twitch.tv/v5bz');
+
+});
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
@@ -9,6 +15,102 @@ client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
   }
+});
+
+﻿﻿client.on("message", message => {
+if(message.content.startsWith(prefix + "avatar")){
+if(message.author.bot || message.channel.type == "dm") return;
+var args = message.content.split(" ")[1];
+var avt = args || message.author.id;
+client.fetchUser(avt)
+.then((user) => {
+avt = user
+let avtEmbed = new Discord.RichEmbed()
+.setColor("#36393e")
+.setAuthor(`${avt.username}'s Avatar`, message.author.avatarURL)
+.setImage(avt.avatarURL)
+.setFooter(`PrimeBot.`, message.client.user.avatarURL);
+message.channel.send(avtEmbed);
+})
+.catch(() => message.channel.send(`Error`));
+} // Julian
+}); // Codes - Toxic Codes
+
+
+client.on("message", msg=>{
+if(!msg.content.startsWith(`${prefix}tax`)) return;
+let tax = msg.content.split(" ")[1]
+let Price = msg.content.split(" ")[2];
+if(!tax || !Price) return msg.reply(`\`${prefix}tax 15% 100000\``).then(z=>z.delete(3000));
+tax = tax.replace(/%/g,"");
+let resulting = Math.floor(Price-(Price*(tax/100)));
+if(!resulting || resulting < 0 ||  isNaN(resulting)) return msg.reply(`\`${prefix}tax 15% 100000\``).then(z=>z.delete(3000));
+msg.reply(`resulting is ${resulting}$`)
+})
+
+
+
+client.on("message", message => {
+    if(message.content.startsWith(prefix + "server")){
+        if(message.author.bot || message.channel.type == "dm") return;
+        let onlineM = message.guild.members.filter(m => m.presence.status !== "offline");
+        let verifyL = ["None", "Low", "Medium", "Hard", "Extreme"];
+        let region = {
+            'brazil': "`Brazil`",
+            'eu-central': "`Central Europe`",
+            'singapore': "`Singapore`",
+            'us-central': "`US Central`",
+            'sydney': "`Sydney`",
+            'us-east': "`US East`",
+            'us-south': "`US South`",
+            'us-west': "`US West`",
+            'eu-west': "`Western Europe`",
+            'london': "`London`",
+            'amsterdam': "`Amsterdam`",
+            'hongkong': "`Hong Kong`",
+            'russia': "`Russia`"
+        };
+        let serverEmbed = new Discord.RichEmbed()
+        .setColor("#36393e")
+        .setThumbnail(message.guild.iconURL)
+        .setAuthor(`${message.guild.name}, Server Info`)
+        .setDescription(`- Server Name \`${message.guild.name}\`\n- Created At \`${moment(message.guild.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Total Members \`${message.guild.memberCount} [Online: ${onlineM.size}]\`\n- Server Owner \`${message.guild.owner.user.tag}\`\n- Channels \`${message.guild.channels.filter(m => m.type == 'text').size} Text || ${message.guild.channels.filter(m => m.type == 'voice').size} Voice\`\n- Categories \`${message.guild.channels.filter(m => m.type == 'category').size}\`\n- Roles \`${message.guild.roles.size}\`\n- Region \`${region[message.guild.region]}\`\n- Verification Level \`${verifyL[message.guild.verificationLevel]}\`\n- Server ID \`${message.guild.id}\``)
+        .setFooter(message.client.user.username,message.client.user.avatarURL);
+    if(!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send(`:no_entry: You dont have permission!`).then(mm => mm.delete(5000));  message.channel.send(serverEmbed).catch(console.error);
+    } else if(message.content.startsWith(prefix + "user")){
+        if(message.author.bot || message.channel.type == "dm") return;
+        let mnt = message.mentions.users.first();
+        let user = mnt || message.author;
+        let userEmbed = new Discord.RichEmbed()
+        .setColor("#36393e")
+        .setThumbnail(user.avatarURL)
+        .setAuthor(`${user.username}, User Info`)
+        .setDescription(`- Name \`${user.tag}\`\n- Created At \`${moment(user.createdAt).format('D/MM/YYYY h:mm a')}\`\n- Joined At \`${moment(user.joinedAt).format('D/MM/YYYY h:mm a')}\`\n- ID \`${user.id}\``)
+        .setFooter(message.client.user.username,message.client.user.avatarURL);
+        message.channel.send(userEmbed).catch(console.error);
+       
+    }
+});
+
+client.on("message", message => {
+if(message.content.startsWith(prefix + "vmute")) {
+var mnt = message.mentions.members.first();
+if(message.author.bot || message.channel.type == "dm" || !message.member.hasPermission("MUTE_MEMBERS") || !message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return;
+if(!mnt) return message.channel.send(`**• | Usage:** ${prefix}vmute \`\`@Name\`\``);
+if(!mnt.voiceChannel) return message.channel.send(`⛔ | *${mnt.user.tag}* is not in a voice channel!`);
+mnt.setMute(true).then(() => {
+message.channel.send(`Successfully Muted ${mnt} :+1:`)
+}).catch(console.error);
+}
+if(message.content.startsWith(prefix + "unvmute")) {
+var mnt = message.mentions.members.first();
+if(message.author.bot || message.channel.type == "dm" || !message.member.hasPermission("MUTE_MEMBERS") || !message.guild.member(client.user).hasPermission("MUTE_MEMBERS")) return;
+if(!mnt) return message.channel.send(`**• | Usage:** ${prefix}unvmute \`\`@Name\`\``);
+if(!mnt.voiceChannel) return message.channel.send(`⛔ | *${mnt.user.tag}* is not in a voice channel!`);
+mnt.setMute(false).then(() => {
+message.channel.send(`Successfully Unmuted ${mnt} :+1:`)
+}).catch(console.error);
+}
 });
 
 client.login(process.env.BOT_TOKEN);
