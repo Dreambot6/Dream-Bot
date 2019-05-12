01f096242d55aa2864d0977ad7b45fc52ab0a11b
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-12 15:50:40 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -885,6 +885,63 @@ var ApL = `${Math.round(client.ping)}`
 
 
 
+client.on("message", message => {
+    var prefix = "=";// البرفكس
+if(message.content.startsWith(prefix + "setwlc")) {
+    let args = message.mentions.channels.first();
+        if(!args) message.channel.send("** منشن روم . :x:**").then(m => {    
+m.delete(1500);
+})
+            if(!message.guild.member(message.author.id).hasPermission("MANAGE_CHANNELS")) return message.channel.send("**ليس لديك صلاحيات . :x:**");
+                    message.channel.send(`**${args}. لقد تم شغل الروم هذا للترحيب.**`);
+                client.on("guildMemberAdd", (member) => {
+                        if(member.user.bot) return;
+                     var embed = new Discord.RichEmbed()
+.setAuthor(member.user.username, member.user.avatarURL)
+.setThumbnail(member.user.avatarURL)
+.setTitle('New Member')
+.setDescription(`Welcome To Server : [ ${message.guild.name} ]`)
+.addField("**اسم العضو** :", `${message.author.tag}`, true)
+.addField('**ايدي العضو** :',"" +  member.user.id, true)
+.addField('**تاج العضو** :', member.user.discriminator, true)
+.addField('**صنع الحساب منذ** :',member.user.createdAt, true)
+.addField('**انت العضو رقم**',`**[ ${member.guild.memberCount} ]**`,true)
+.setColor('GREEN')
+.setFooter(member.guild.name, member.guild.iconURL, true)
+                     
+args.send({embed : embed});
+                });
+}
+});
+
+client.on("message", message => {
+    var prefix = "=";//البرفكس
+if(message.content.startsWith(prefix + "setout")) {
+    let args = message.mentions.channels.first();
+        if(!args) message.channel.send("** منشن روم . :x:**");
+            if(!message.guild.member(message.author.id).hasPermission("MANAGE_CHANNELS")) return message.channel.send("**ليس لديك صلاحيات . :x:**");
+                    message.channel.send(`**${args}. لقد تم شغل الروم هذا للترحيب.**`);
+                client.on("guildMemberRemove", (member) => {
+                        if(member.user.bot) return;
+                     var embed = new Discord.RichEmbed()
+.setAuthor(member.user.username, member.user.avatarURL)
+.setThumbnail(member.user.avatarURL)
+.setTitle('Out Member')
+.setDescription(`GoodBye From Server : [ ${message.guild.name} ]`)
+.addField("**اسم العضو** :", `${message.author.username}#${message.author.discriminator}`, true)
+.addField('**ايدي العضو** :',"" +  member.user.id, true)
+.addField('**تاج العضو** :', member.user.discriminator, true)
+.addField('**صنع الحساب منذ** :',member.user.createdAt, true)
+.addField('**انت العضو رقم**',`**[ ${member.guild.memberCount} ]**`,true)
+.setColor('RED')
+.setFooter(member.guild.name, member.guild.iconURL, true)
+                     
+args.send({embed : embed});
+                });
+}
+});
+
+
 
 
 
