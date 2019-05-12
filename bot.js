c58cb2d62875be031ced9e5f5a5caae580ec4d16
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-12 16:16:37 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1172,41 +1172,70 @@ client.on('message', async message => {
 
 
 
-//help
-client.on("message", message => {
-             
-     if(!message.channel.guild) return;
+client.on("message", (message) => {
+  let men = message.mentions.users.first()
 
- if (message.content === ">help") {
-   message.react("✅")
-  const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(message.author.avatarURL)
-      .setDescription(`
-**Helping Commands :**
+  if (message.author.bot) return;
+    if (message.author.id === client.user.id) return;
+	if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + 'credit')) {
+  if(men) {
+    if (!profile[men.id]) profile[men.id] = {
+    lastDaily:'Not Collected',
+    credits: 1,
+  };
+  }
+  if(men) {
+message.channel.send(`** ${men.username}, :credit_card: balance` + " is `" + `${profile[men.id].credits}$` + "`.**")
+} else {
+  message.channel.send(`** ${message.author.username}, your :credit_card: balance` + " is `" + `${profile[message.author.id].credits}$` + "`.**")
+}
+}
 
-**❖ Admin Help ➾** >help admin
+if(message.content.startsWith(prefix + "daily")) {
+ if(profile[message.author.id].lastDaily != moment().format('day')) {
+    profile[message.author.id].lastDaily = moment().format('day')
+    profile[message.author.id].credits += 300
+     message.channel.send(`**${message.author.username} you collect your \`300\` :dollar: daily pounds**`)
+} else {
+    message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
+}
+  }
 
-**❖ General Help ➾** >help general
+ let cont = message.content.slice(prefix.length).split(" ");
+let args = cont.slice(1);
+let sender = message.author
+if(message.content.startsWith(prefix + 'trans')) {
+          if (!args[0]) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+         return;
+           }
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+             }
+            let defineduser = '';
+            let firstMentioned = message.mentions.users.first();
+            defineduser = (firstMentioned)
+            if (!defineduser) return message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
+            var mentionned = message.mentions.users.first();
+if (!profile[sender.id]) profile[sender.id] = {}
+if (!profile[sender.id].credits) profile[sender.id].credits = 200;
+fs.writeFile('./profile.json', JSON.stringify(profile), (err) => {
+if (err) console.error(err);
+})
+      var mando = message.mentions.users.id;
+      if  (!profile[defineduser.id]) profile[defineduser.id] = {}
+      if (!profile[defineduser.id].credits) profile[defineduser.id].credits = 200;
+      profile[defineduser.id].credits += (+args[0]);
+      profile[sender.id].credits += (-args[0]);
+      let mariam = message.author.username
+message.channel.send(`**:moneybag: | ${message.author.username}, has transferrerd ` + "`" + args[0] + "$` to " + `<@${defineduser.id}>**`)
+}
 
-**❖ Games Help ➾** >help games
+      });
 
-**❖ Quran Help ➾** >help quran
-
-**❖ Soundboard Help ➾** >help sb
-
-**❖ Music Help ➾** >help Music
-
-**❖ Important Help ➾** >help important
-**.**
-`)
-       .setTimestamp()
-       .setFooter(message.author.username, message.author.avatarURL)
-
-  message.channel.sendMessage({embed});
-
- }
-});
 
 
 
