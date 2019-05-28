b54c98c95444cd80684d724f62fb8b08aba0defe
Dreambot6 <50480297+Dreambot6@users.noreply.github.com>
2019-05-28 08:07:17 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -787,8 +787,8 @@ client.on('message', message => {
  message.author.sendMessage(`
  
  __~~Bot Staff~~__
-@! Dream ! hamodii_yt#8500  @!̤ D̲͙ͅrea͊ͣ̿m ̨! ON#7991 
- __Powered By__: @! Dream ! hamodii_yt#8500  @!̤ D̲͙ͅrea͊ͣ̿m ̨! ON#7991  
+@! Dream ! hamodii_yt#8500   
+ __Powered By__: @! Dream ! hamodii_yt#8500  
 Server Support : https://discord.gg/Nbt8yxY
 `);
  
